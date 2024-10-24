1657bcbec5d369254f2e9ecfca8c355a8cc7a62c
ginloy <liyongleow@gmail.com>
2024-10-24 10:13:16 +0000

    Add more to Controller skeleton

--- src/ee3305_nav/include/ee3305_nav/controller.hpp
+++ src/ee3305_nav/include/ee3305_nav/controller.hpp
@@ -4,11 +4,28 @@
 #include "rclcpp/rclcpp.hpp"
 
 namespace ee3305 {
-    class Controller: public rclcpp::Node {
-        private:
-        public:
-        private:
-    };
-}
+class Controller : public rclcpp::Node {
+private:
+  // States and ROS2 Parameters, Actions, Topics, Timers and Services
+
+public:
+  // Constructors, Destructors, etc..
+
+  explicit Controller();
+
+private:
+  // Initializations for ROS2 PATTs and states
+  // Callback definitions required for any ROS2 PATTs
+
+  void initStates();
+  void initParams();
+  void initTopics();
+  void initServices();
+  void initTimers();
+
+public:
+  // Other public definitions
+};
+} // namespace ee3305
 
 #endif
--- src/ee3305_nav/src/controller.cpp
+++ src/ee3305_nav/src/controller.cpp
@@ -1,6 +1,41 @@
 #include "ee3305_nav/controller.hpp"
-#include "rclcpp/rclcpp.hpp"
+#include <memory>
+#include <rclcpp/node.hpp>
 
-int main() {
+using namespace ee3305;
 
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ee3305::Controller>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
+
+Controller::Controller(): rclcpp::Node("controller") {
+    initStates();
+    initParams();
+    initTopics();
+    initServices();
+    initTimers();
+}
+
+void Controller::initStates() {
+    // TODO
+}
+
+void Controller::initParams() {
+    // TODO
+}
+
+void Controller::initServices() {
+    // TODO
+}
+
+void Controller::initTopics() {
+    // TODO
+}
+
+void Controller::initTimers() {
+    // TODO
 }
