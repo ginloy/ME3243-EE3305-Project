3969169667e9a2431ee065dd3c48f55e2a5d051d
ginloy <liyongleow@gmail.com>
2024-10-24 04:49:05 +0000

    Setup skeleton for controller

---
+++ src/ee3305_nav/include/ee3305_nav/controller.hpp
@@ -0,0 +1,14 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace ee3305 {
+    class Controller: public rclcpp::Node {
+        private:
+        public:
+        private:
+    };
+}
+
+#endif
---
+++ src/ee3305_nav/src/controller.cpp
@@ -0,0 +1,6 @@
+#include "ee3305_nav/controller.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+int main() {
+
+}
