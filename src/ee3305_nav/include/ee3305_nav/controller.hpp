c69b51e371dc87333d8690cb8d3d6b5798f66667
ginloy <liyongleow@gmail.com>
2024-10-29 02:32:11 +0000

    Include provided controller.cpp template

--- src/ee3305_nav/include/ee3305_nav/controller.hpp
+++ src/ee3305_nav/include/ee3305_nav/controller.hpp
@@ -1,12 +1,37 @@
 #ifndef CONTROLLER_HPP
 #define CONTROLLER_HPP
 
-#include "rclcpp/rclcpp.hpp"
+#include <ee3305_nav/ee3305_nav.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/detail/odometry__struct.hpp>
+#include <nav_msgs/msg/detail/path__struct.hpp>
+#include <rclcpp/rclcpp.hpp>
 
 namespace ee3305 {
 class Controller : public rclcpp::Node {
 private:
   // States and ROS2 Parameters, Actions, Topics, Timers and Services
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom;
+  rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr sub_path;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_vel;
+
+  // ----------- Timers -------------------
+  rclcpp::TimerBase::SharedPtr
+      timer_main; // contains the timer that runs the main looping function at
+                  // regular intervals.
+
+  // ----------- Parameters ---------------
+  double frequency;
+  double lookahead_distance;
+  double stop_thres;
+  double lookahead_lin_vel;
+
+  // ----------- States / Others -------------
+  std::vector<double> plan_flat;
+  double rbt_x;
+  double rbt_y;
+  double rbt_h;
 
 public:
   // Constructors, Destructors, etc..
@@ -22,6 +47,13 @@ private:
   void initTopics();
   void initServices();
   void initTimers();
+
+  void cbPath(nav_msgs::msg::Path::SharedPtr msg);
+
+  // transform assumes zero transform between `map` frame and `odom` frame.
+  void cbOdom(nav_msgs::msg::Odometry::SharedPtr msg);
+
+  void cbTimerMain();
 
 public:
   // Other public definitions
--- src/ee3305_nav/src/controller.cpp
+++ src/ee3305_nav/src/controller.cpp
@@ -1,6 +1,9 @@
 #include "ee3305_nav/controller.hpp"
+#include <chrono>
 #include <memory>
-#include <rclcpp/node.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+using namespace std::chrono_literals;
 
 using namespace ee3305;
 
@@ -12,30 +15,77 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-Controller::Controller(): rclcpp::Node("controller") {
-    initStates();
-    initParams();
-    initTopics();
-    initServices();
-    initTimers();
+Controller::Controller() : rclcpp::Node("controller") {
+  initStates();
+  initParams();
+  initTopics();
+  initServices();
+  initTimers();
 }
 
 void Controller::initStates() {
-    // TODO
+  rbt_x = NAN;
+  rbt_y = NAN;
+  rbt_h = NAN;
 }
 
 void Controller::initParams() {
-    // TODO
+  initParam(this, "frequency", frequency);
+  initParam(this, "lookahead_distance", lookahead_distance);
+  initParam(this, "stop_thres", stop_thres);
+  initParam(this, "lookahead_lin_vel", lookahead_lin_vel);
 }
 
 void Controller::initServices() {
-    // TODO
+  timer_main = this->create_wall_timer(
+      1s / frequency, std::bind(&Controller::cbTimerMain, this));
 }
 
 void Controller::initTopics() {
-    // TODO
+  sub_path = this->create_subscription<nav_msgs::msg::Path>(
+      "path", rclcpp::SensorDataQoS(),
+      std::bind(&Controller::cbPath, this, std::placeholders::_1));
+
+  sub_odom = this->create_subscription<nav_msgs::msg::Odometry>(
+      "odom", rclcpp::SensorDataQoS(),
+      std::bind(&Controller::cbOdom, this, std::placeholders::_1));
+
+  pub_cmd_vel = this->create_publisher<geometry_msgs::msg::Twist>(
+      "cmd_vel", rclcpp::SystemDefaultsQoS());
 }
 
 void Controller::initTimers() {
-    // TODO
+  // TODO
+}
+
+void Controller::cbPath(nav_msgs::msg::Path::SharedPtr msg) {
+  plan_flat.clear();
+  for (const geometry_msgs::msg::PoseStamped &pose : msg->poses) {
+    plan_flat.push_back(pose.pose.position.x);
+    plan_flat.push_back(pose.pose.position.y);
+  }
+}
+
+void Controller::cbOdom(nav_msgs::msg::Odometry::SharedPtr msg) {
+  rbt_x = msg->pose.pose.position.x;
+  rbt_y = msg->pose.pose.position.y;
+  const auto &q = msg->pose.pose.orientation;
+  double siny_cosp = 2 * (q.w * q.z + q.x * q.y);
+  double cosy_cosp = 1 - 2 * (q.y + q.y + q.z * q.z);
+  rbt_h = atan2(siny_cosp, cosy_cosp);
+}
+
+void Controller::cbTimerMain() {
+  double ang_vel = 0.0; // TODO
+  double lin_vel = 0.0; // TODO
+
+  // Publish message
+  geometry_msgs::msg::Twist msg;
+  msg.angular.x = 0; // redundancy
+  msg.angular.y = 0; // redundancy
+  msg.angular.z = ang_vel;
+  msg.linear.x = lin_vel;
+  msg.linear.y = 0; // redundancy
+  msg.linear.z = 0; // redundancy
+  pub_cmd_vel->publish(msg);
 }
